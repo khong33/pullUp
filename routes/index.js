85be65653762dba00018b9c76ae08bc5ce778d04
Leo Son <lson@onetrust.com>
2018-09-12 22:30:17 -0400

    Testing ronald's git skills

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'bhumi' });
 });
+
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'bhumi' });
+});
+
+
+router.get('/availability', function(req, res, next) {
+  res.render('availability', { title: 'availability' });
+});
+
 
 module.exports = router;
